70f32f92f04f7c98c8b20075f42e8584d7860f10
NikolayRaynov <niksan.nr@gmail.com>
2025-01-18 23:22:20 +0200

    Add FavoritePlaces model

---
+++ Travel_Info.Data.Models/FavoritePlaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Data.Models
+{
+    public class FavoritePlaces
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; } = null!;
+        public virtual ICollection<Destination> Destinations { get; set; } = new HashSet<Destination>();
+    }
+}
