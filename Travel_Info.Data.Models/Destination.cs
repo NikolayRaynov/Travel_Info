674978b7837ff3d9168b066b500b64394d17429e
Nikolay <niksa.nr@gmail.com>
2025-01-18 21:04:02 +0200

    Add Destination model

---
+++ Travel_Info.Data.Models/Destination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Data.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        public virtual ICollection<Image> Images { get; set; } = new List<Image>();
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+        public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
