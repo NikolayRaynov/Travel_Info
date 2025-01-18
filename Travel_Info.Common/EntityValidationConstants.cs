b40881caac3ed5d6b604d0d6a858b11eddb63ef6
NikolayRaynov <niksan.nr@gmail.com>
2025-01-18 20:47:18 +0200

    Add validation constants to model

---
+++ Travel_Info.Common/EntityValidationConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class Image
+        {
+            public const int UrlMinLength = 8;
+            public const int UrlMaxLength = 2083;
+            public const int DescriptionMinLength = 10;
+            public const int DescriptionMaxLength = 500;
+        }
+    }
+}
