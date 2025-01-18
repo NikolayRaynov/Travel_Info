0abe82af970c79fc3d92ebbc93a4fc34e21668ba
Nikolay <niksa.nr@gmail.com>
2025-01-18 23:38:53 +0200

    Add relations from ApplicationUser to PlacesToVisit and FavoritePlaces entities

--- Travel_Info.Data.Models/ApplicationUser.cs
+++ Travel_Info.Data.Models/ApplicationUser.cs
@@ -10,5 +10,7 @@ namespace Travel_Info.Data.Models
     public class ApplicationUser : IdentityUser
     {
         public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+        public virtual ICollection<PlacesToVisit> PlacesToVisit { get; set; } = new List<PlacesToVisit>();
+        public virtual ICollection<FavoritePlaces> FavoritePlaces { get; set; } = new List<FavoritePlaces>();
     }
 }
