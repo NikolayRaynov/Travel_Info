abc1a124313f1ef58b164d88fd8ac49be222564a
NikolayRaynov <niksan.nr@gmail.com>
2025-01-18 23:20:33 +0200

    Add model for visited places

---
+++ Travel_Info.Data.Models/PlacesToVisit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Data.Models
+{
+    public class PlacesToVisit
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; } = null!;
+        public virtual ICollection<Destination> Destinations { get; set; } = new HashSet<Destination>();
+    }
+}
