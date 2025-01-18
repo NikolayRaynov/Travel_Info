30b73b2db2466b2d69905a5a2d561ff0f37803cb
NikolayRaynov <niksan.nr@gmail.com>
2025-01-18 20:36:18 +0200

    Add Image entity

---
+++ Travel_Info.Data.Models/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Data.Models
+{
+    public class Image
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int DestinationId { get; set; }
+
+        [Required]
+        public string Url { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        public virtual Destination Destination { get; set; } = null!;
+    }
+}
