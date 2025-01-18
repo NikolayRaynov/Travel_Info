1ab35b5f2199bfd8a8e0076dd58956d935b20071
NikolayRaynov <niksan.nr@gmail.com>
2025-01-18 20:55:42 +0200

    Add Category model

---
+++ Travel_Info.Data.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel_Info.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Destination> Destinations { get; set; }
+
+    }
+}
