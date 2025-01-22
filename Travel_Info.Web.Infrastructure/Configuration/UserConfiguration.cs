ba69f60fb02138e03648b672eb4172d6f9e1cad7
Nikolay <niksa.nr@gmail.com>
2025-01-22 19:05:50 +0200

    Seed Users

---
+++ Travel_Info.Web.Infrastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travel_Info.Data.Models;
+
+namespace Travel_Info.Web.Infrastructure.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasData(SeedUsers());
+        }
+
+        private List<ApplicationUser> SeedUsers()
+        {
+            var users = new List<ApplicationUser>();
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            var user = new ApplicationUser()
+            {
+                Id = "dea12856-c198-4129-b3f3-b893d8395082",
+                UserName = "user@mail.com",
+                NormalizedUserName = "USER@MAIL.COM",
+                Email = "user@mail.com",
+                NormalizedEmail = "USER@MAIL.COM"
+            };
+
+            user.PasswordHash =
+                 hasher.HashPassword(user, "user123");
+
+            users.Add(user);
+
+            user = new ApplicationUser()
+            {
+                Id = "8b3d2e65-4498-4d45-9127-2fde83fef2a4",
+                UserName = "admin@mail.com",
+                NormalizedUserName = "ADMIN@MAIL.COM",
+                Email = "admin@mail.com",
+                NormalizedEmail = "ADMIN@MAIL.COM"
+            };
+
+            user.PasswordHash =
+            hasher.HashPassword(user, "Admin1234");
+
+            users.Add(user);
+
+            return users;
+        }
+    }
+}
