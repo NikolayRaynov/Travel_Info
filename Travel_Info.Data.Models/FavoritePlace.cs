71169681a6b37be77e79aac0eff3a46a8853b831
NikolayRaynov <niksan.nr@gmail.com>
2025-01-25 20:03:57 +0200

    Add comments to the other models

--- Travel_Info.Data.Models/FavoritePlace.cs
+++ Travel_Info.Data.Models/FavoritePlace.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Travel_Info.Data.Models
@@ -9,10 +10,12 @@ namespace Travel_Info.Data.Models
         public int Id { get; set; }
 
         [Required]
+        [Comment("Identifier of the user who marked the place as favorite.")]
         public string UserId { get; set; } = string.Empty;
         [ForeignKey(nameof(UserId))]
         public virtual ApplicationUser User { get; set; } = null!;
 
+        [Comment("Indicator for logical deletion of the favorite place.")]
         public bool IsDeleted { get; set; } = false;
         public virtual ICollection<Destination> Destinations { get; set; } = new HashSet<Destination>();
     }
--- Travel_Info.Data.Models/Review.cs
+++ Travel_Info.Data.Models/Review.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static Travel_Info.Common.EntityValidationConstants.Review;
 
@@ -9,24 +10,30 @@ namespace Travel_Info.Data.Models
         [Key]
         public int Id { get; set; }
 
+        [Comment("Rating given to the destination.")]
         public int Rating { get; set; }
 
         [MaxLength(CommentMaxLength)]
+        [Comment("Comment from the user about the destination.")]
         public string? Comment { get; set; }
 
         [Required]
+        [Comment("Date when the review was created.")]
         public DateTime CreatedAt { get; set; }
 
         [Required]
+        [Comment("Identifier of the user who wrote the review.")]
         public string UserId { get; set; } = string.Empty;
         [ForeignKey(nameof(UserId))]
         public virtual ApplicationUser User { get; set; } = null!;
 
         [Required]
+        [Comment("Identifier of the destination for which the review is written.")]
         public int DestinationId { get; set; }
         [ForeignKey(nameof(DestinationId))]
         public virtual Destination Destination { get; set; } = null!;
 
+        [Comment("Indicator for logical deletion of the review.")]
         public bool IsDeleted { get; set; } = false;
     }
 }
