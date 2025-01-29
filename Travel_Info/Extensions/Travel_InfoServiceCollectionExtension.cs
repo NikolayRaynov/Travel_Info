6a054b9c48f22f359d128066a372089c4b9e044a
NikolayRaynov <niksan.nr@gmail.com>
2025-01-29 22:24:17 +0200

    Create an extension to add servers in the application

---
+++ Travel_Info/Extensions/Travel_InfoServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Travel_Info.Data.Repository;
+using Travel_Info.Data.Repository.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class Travel_InfoServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+
+            return services;
+        }
+    }
+}
