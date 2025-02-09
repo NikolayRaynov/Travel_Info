4ed9176ce554be931c814bef93012d9e62b9bdca
Nikolay <niksa.nr@gmail.com>
2025-02-09 14:27:17 +0200

    Add PlaceToViist service

---
+++ Travel_Info.Services.Data/Interfaces/IPlaceToVisitService.cs
@@ -0,0 +1,12 @@
+﻿using Travel_Info.Data.Models;
+
+namespace Travel_Info.Services.Data.Interfaces
+{
+    public interface IPlaceToVisitService
+    {
+        Task<bool> AddToWishlistAsync(int destinationId, string userId);
+        Task RemoveFromWishlistAsync(int destinationId, string userId);
+        Task<bool> IsInWishlistAsync(int destinationId, string userId);
+        Task<IEnumerable<Destination>> GetAllDesiredPlacesAsync(string userId);
+    }
+}
---
+++ Travel_Info.Services.Data/PlaceToVisitService.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Travel_Info.Data.Models;
+using Travel_Info.Data.Repository.Interfaces;
+using Travel_Info.Services.Data.Interfaces;
+
+namespace Travel_Info.Services.Data
+{
+    public class PlaceToVisitService : IPlaceToVisitService
+    {
+        private readonly IRepository repository;
+
+        public PlaceToVisitService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<bool> AddToWishlistAsync(int destinationId, string userId)
+        {
+            var existingDesiredPlace = await repository.All<PlaceToVisit>()
+                .Include(pv => pv.Destinations)
+                .FirstOrDefaultAsync(pv => pv.UserId == userId && pv.Destinations.Any(d => d.Id == destinationId));
+
+            if (existingDesiredPlace != null)
+            {
+                return false;
+            }
+
+            var userFavorites = await repository.All<PlaceToVisit>()
+                .FirstOrDefaultAsync(pv => pv.UserId == userId);
+
+            if (userFavorites == null)
+            {
+                userFavorites = new PlaceToVisit
+                {
+                    UserId = userId,
+                    Destinations = new List<Destination>()
+                };
+
+                await repository.AddAsync(userFavorites);
+            }
+
+            var destination = await repository.GetByIdAsync<Destination>(destinationId);
+            if (destination != null)
+            {
+                userFavorites.Destinations.Add(destination);
+            }
+
+            await repository.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<Destination>> GetAllDesiredPlacesAsync(string userId)
+        {
+            return await repository
+                .All<PlaceToVisit>()
+                .Where(pv => pv.UserId == userId && !pv.IsDeleted)
+                .Include(pv => pv.Destinations)
+                .ThenInclude(i => i.Images)
+                .SelectMany(pv => pv.Destinations)
+                .ToListAsync();
+
+        }
+
+        public async Task<bool> IsInWishlistAsync(int destinationId, string userId)
+        {
+            return await repository
+                .All<PlaceToVisit>()
+                .AnyAsync(w => w.UserId == userId && w.Destinations.Any(d => d.Id == destinationId) && !w.IsDeleted);
+        }
+
+        public async Task RemoveFromWishlistAsync(int destinationId, string userId)
+        {
+            var desiredPlaces = await repository
+                .All<PlaceToVisit>()
+                .Include(pv => pv.Destinations)
+                .FirstOrDefaultAsync(pv => pv.UserId == userId && !pv.IsDeleted);
+
+            if (desiredPlaces == null)
+            {
+                throw new InvalidOperationException("This destination is not in your wishlist");
+            }
+
+            var destinationToRemove = desiredPlaces.Destinations.FirstOrDefault(d => d.Id == destinationId);
+            if (destinationToRemove != null)
+            {
+                desiredPlaces.Destinations.Remove(destinationToRemove);
+            }
+
+            if (!desiredPlaces.Destinations.Any())
+            {
+                desiredPlaces.IsDeleted = true;
+            }
+
+            repository.Update(desiredPlaces);
+            await repository.SaveChangesAsync();
+        }
+    }
+}
