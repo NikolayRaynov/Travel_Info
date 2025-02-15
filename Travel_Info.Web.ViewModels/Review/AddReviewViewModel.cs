d2f2d48b3888ef1bea6b2363ec45d7bdfe647938
NikolayRaynov <niksan.nr@gmail.com>
2025-02-15 22:35:52 +0200

    Create review model for add

---
+++ Travel_Info.Web.ViewModels/Review/AddReviewViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Travel_Info.Web.ViewModels.Review
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Travel_Info.Common.EntityValidationConstants.Review;
+    public class AddReviewViewModel
+    {
+        public int DestinationId { get; set; }
+        public int Rating { get; set; }
+
+        [MaxLength(CommentMaxLength)]
+        public string Comment { get; set; } = string.Empty;
+    }
+}
