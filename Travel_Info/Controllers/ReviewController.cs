81840530d2a32e82d4727bc7157e4b64c360f242
Nikolay <niksa.nr@gmail.com>
2025-02-15 22:27:25 +0200

    Create ReviewController and implement Index action

---
+++ Travel_Info/Controllers/ReviewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travel_Info.Services.Data;
+using Travel_Info.Services.Data.Interfaces;
+using Travel_Info.Web.ViewModels.Review;
+
+namespace Travel_Info.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly IReviewService reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            this.reviewService = reviewService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int destinationId)
+        {
+            var reviews = await reviewService.GetAllReviewsByDestinationIdAsync(destinationId);
+            var model = reviews.Select(r => new ReviewViewModel
+            {
+                Id = r.Id,
+                Rating = r.Rating,
+                Comment = r.Comment,
+                CreatedAt = r.CreatedAt,
+                User = r.User.UserName
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
