3f8796d0c1d8169a9ee76e2b9bcf98c59c47db5d
Nikolay <niksa.nr@gmail.com>
2025-02-16 17:28:21 +0200

    Add view for Review controller

--- Travel_Info.Web.ViewModels/Review/AddReviewViewModel.cs
+++ Travel_Info.Web.ViewModels/Review/AddReviewViewModel.cs
@@ -1,10 +1,16 @@
 ﻿namespace Travel_Info.Web.ViewModels.Review
 {
     using System.ComponentModel.DataAnnotations;
+    using static Travel_Info.Common.EntityValidationConstants.Destination;
     using static Travel_Info.Common.EntityValidationConstants.Review;
     public class AddReviewViewModel
     {
         public int DestinationId { get; set; }
+
+        [Required]
+        [MinLength(DestinationNameMinLength)]
+        [MaxLength(DestinationNameMaxLength)]
+        public string DestinationName { get; set; } = null!;
         public int Rating { get; set; }
 
         [MaxLength(CommentMaxLength)]
