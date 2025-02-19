c3c61059fe7fd5386a395b0812d8bf6fcb5b2f13
Nikolay <niksa.nr@gmail.com>
2025-02-19 21:11:35 +0200

    add Edit view

--- Travel_Info.Web.ViewModels/Review/ReviewViewModel.cs
+++ Travel_Info.Web.ViewModels/Review/ReviewViewModel.cs
@@ -3,6 +3,7 @@
     public class ReviewViewModel
     {
         public int Id { get; set; }
+        public int DestinationId { get; set; }
         public int Rating { get; set; }
         public string? Comment { get; set; }
         public DateTime CreatedAt { get; set; }
