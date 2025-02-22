7754392c127931facb5c8a6811546d545291c285
NikolayRaynov <niksan.nr@gmail.com>
2025-02-22 13:57:27 +0200

    Add ViewModel for Delete action in Review controller

---
+++ Travel_Info.Web.ViewModels/Review/DeleteReviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Travel_Info.Web.ViewModels.Review
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Travel_Info.Common.EntityValidationConstants.Review;
+    public class DeleteReviewViewModel
+    {
+        public int Id { get; set; }
+        public int DestinationId { get; set; }
+        public int Rating { get; set; }
+
+        [MaxLength(CommentMaxLength)]
+        public string? Comment { get; set; }
+    }
+}
