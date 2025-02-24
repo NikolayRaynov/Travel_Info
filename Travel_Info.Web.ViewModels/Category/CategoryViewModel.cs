57d49b6b3d6d4d98c9f3811e51a9a46d4f6fec75
NikolayRaynov <niksan.nr@gmail.com>
2025-02-24 19:34:35 +0200

    Add CategoryViewModel

---
+++ Travel_Info.Web.ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Travel_Info.Web.ViewModels.Category
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string NameBg { get; set; } = string.Empty;
+        public string NameEn { get; set; } = string.Empty;
+    }
+}
