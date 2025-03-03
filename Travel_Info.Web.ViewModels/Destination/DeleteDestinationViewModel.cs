c420bc0ca5cedd6cb781609895f1172939f01174
NikolayRaynov <niksan.nr@gmail.com>
2025-03-03 14:25:49 +0200

    Add DeleteDestinationViewModel

---
+++ Travel_Info.Web.ViewModels/Destination/DeleteDestinationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Travel_Info.Web.ViewModels.Destination
+{
+    public class DeleteDestinationViewModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<string> ImageUrls { get; set; } = new List<string>();
+    }
+}
