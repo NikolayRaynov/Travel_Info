ed6d8a611b66539aeab1d8369c049f70fe8d6e15
Nikolay <niksa.nr@gmail.com>
2025-03-30 18:23:48 +0300

    Add Username properties

--- Travel_Info.Common/ApplicationConstants.cs
+++ Travel_Info.Common/ApplicationConstants.cs
@@ -5,5 +5,7 @@
         public const int ReleaseYear = 2025;
         public const string DefaultFolder = "wwwroot";
         public const string UrlPath = "wwwroot/images/";
+        public const int UsernameMinLength = 4;
+        public const int UsernameMaxLength = 255;
     }
 }
