557763b5c4384f5b92ba0501c6a9dec69d9fc9eb
NikolayRaynov <niksan.nr@gmail.com>
2025-04-06 02:05:11 +0300

    Add username to register form

--- Travel_Info/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Travel_Info/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -74,6 +74,7 @@ namespace Travel_Info.Areas.Identity.Pages.Account
                           ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
                           MinimumLength = UsernameMinLength)]
             public string Username { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
