3114bba1b6a89e8d2e7b04c894da3807c2a1ea19
NikolayRaynov <niksan.nr@gmail.com>
2025-05-17 17:56:18 +0300

    Implement sending confirmation account email functionality

---
+++ Travel_Info.Services.Data/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+
+namespace Travel_Info.Services.Data
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var senderEmail = configuration["EmailSettings:SenderEmail"];
+            var smtpUsername = configuration["EmailSettings:SmtpUsername"];
+            var smtpPassword = configuration["EmailSettings:SmtpPassword"];
+
+            var emailToSend = new MimeMessage();
+
+            emailToSend.From.Add(MailboxAddress.Parse(senderEmail));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = htmlMessage
+            };
+
+            using (var emailClient = new MailKit.Net.Smtp.SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit
+                    .Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate(smtpUsername, smtpPassword);
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
