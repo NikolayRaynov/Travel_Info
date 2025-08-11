49e6daf1a7793d56ad7212677c192c1bec9c19c1
Nikolay <niksan.nr@gmail.com>
2025-08-11 16:56:48 +0300

    Format ConfigurationConstants strings

--- Travel_Info.Common/ApplicationConstants.cs
+++ Travel_Info.Common/ApplicationConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Travel_Info.Common
 {
-    public class ApplicationConstants
+    public static class ApplicationConstants
     {
         public const int ReleaseYear = 2025;
         public const string DefaultFolder = "wwwroot";
